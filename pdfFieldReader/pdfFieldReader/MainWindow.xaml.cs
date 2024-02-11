95c3dac2dfec1a35d1d21546bd58ce43817b9f7c
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2024-02-11 14:04:55 -0500

    File Path Dialog

--- pdfFieldReader/pdfFieldReader/MainWindow.xaml.cs
+++ pdfFieldReader/pdfFieldReader/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace pdfFieldReader
 {
@@ -19,6 +20,20 @@ namespace pdfFieldReader
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_selectPdf_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "PDF Document | *.pdf";
+            
+            bool? file_success = fileDialog.ShowDialog();
+            if(file_success == true)
+            {
+                string file_path = fileDialog.FileName;
+                string file_name = fileDialog.SafeFileName;
+                tb_topleft.Text = file_name + ": " + file_path;
+            }
         }
     }
 }
