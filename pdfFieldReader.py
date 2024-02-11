f01a11a47258d32dfdf74fd98a47f55ac96a7765
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2024-02-10 21:22:28 -0500

    Exception Handling

--- pdfFieldReader.py
+++ pdfFieldReader.py
@@ -8,14 +8,16 @@ def extract_pdf_fields(file_path):
 
         for page_num in range(doc_reader.numPages):
             page = doc_reader.getPage(page_num)
-            fields = page.get('/Annots', [])
-            for field in fields:
-                field_obj = field.getObject()
-                if field_obj.get('/FT') == '/Tx':
-                    field_name = field_obj.get('/T')
-                    field_value = field_obj.get('/V')
-                    fields_dict[field_name] = field_value
-        
+            try:
+                fields = page['/Annots']
+                for field in fields:
+                    field_obj = field.getObject()
+                    if field_obj.get('/FT') == '/Tx':
+                        field_name = field_obj.get('/T')
+                        field_value = field_obj.get('/V')
+                        fields_dict[field_name] = field_value
+            except KeyError:
+                pass
         return fields_dict
 
 fields_dict = extract_pdf_fields(test_path.file)
